122d9511bca7d42a3011b32402761fd28f341f5e
Yonatan Israeli <yonirisraeli@gmail.com>
2024-09-05 21:12:59 -0400

    Update Windows Corruption Fixer.bat

--- Windows Corruption Fixer.bat
+++ Windows Corruption Fixer.bat
@@ -2,7 +2,7 @@
 setlocal
 title Windows Corruption Fixer
 echo Program Name: Windows Corruption Fixer
-echo Version: 6.0.13
+echo Version: 6.0.14
 echo Developer: @YonatanReuvenIsraeli
 echo Website: https://www.yonatanreuvenisraeli.dev
 echo License: GNU General Public License v3.0
@@ -114,6 +114,8 @@ set Media=
 set /p Media="Do you want to use a Windows Disk Image? (Yes/No) "
 if /i "%Media%"=="Yes" goto "DriveLetter"
 if /i "%Media%"=="No" goto "DISMUpdateCheck"
+echo Invalid Sytax!
+goto "Media"
 
 :"DriveLetter"
 echo.
