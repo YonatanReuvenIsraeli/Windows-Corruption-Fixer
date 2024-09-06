ec95e8514365319a80984b1e10d89eced2523c6b
Yonatan Israeli <yonirisraeli@gmail.com>
2024-09-06 15:35:31 -0400

    Update Windows Corruption Fixer.bat

--- Windows Corruption Fixer.bat
+++ Windows Corruption Fixer.bat
@@ -2,7 +2,7 @@
 setlocal
 title Windows Corruption Fixer
 echo Program Name: Windows Corruption Fixer
-echo Version: 8.4.9
+echo Version: 8.4.10
 echo Developer: @YonatanReuvenIsraeli
 echo Website: https://www.yonatanreuvenisraeli.dev
 echo License: GNU General Public License v3.0
@@ -979,7 +979,7 @@ if /i "%OnlineOffline%"=="No" goto "Start"
 echo Invalid syntax!
 goto "10"
 
-:SFCLogsOnline
+:SFCVeiwLogs"
 "%windir%\Logs\CBS\CBS.log"
 goto "Start"
 
