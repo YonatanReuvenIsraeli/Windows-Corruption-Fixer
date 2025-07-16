40d014c78894be58055b7d0be4a6f78eaca96919
Yonatan Israeli <yonirisraeli@gmail.com>
2025-07-16 15:36:21 -0700

    Update Windows Corruption Fixer.bat

--- Windows Corruption Fixer.bat
+++ Windows Corruption Fixer.bat
@@ -2,7 +2,7 @@
 title Windows Corruption Fixer
 setlocal
 echo Program Name: Windows Corruption Fixer
-echo Version: 14.0.3
+echo Version: 14.0.4
 echo License: GNU General Public License v3.0
 echo Developer: @YonatanReuvenIsraeli
 echo GitHub: https://github.com/YonatanReuvenIsraeli
